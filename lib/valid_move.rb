f21f51377e196090cfd7f02878b18bf4922e148d
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-14 18:03:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,41 @@
+require_relative '../spec/spec_helper.rb'
+
 # code your #valid_move? method here
+#def valid_move?(valid_choice(board, index), position_taken(board, index))
+#  if valid_choice? = true && position_taken = false
+#    "Congrats!"
+#  end
+#end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else 
+    true
+  end
+end
+
+
+
+# code valid_board choice method here
+#def valid_choice?(board, index)
+#  if index.between?(0, 8)
+#    true 
+#  else
+#    false
+#  end
+#end 
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+#def position_taken?(board, index)
+#  if board[index] == " " || board[index] == "" || board[index] == nil
+#    false
+#  else 
+#    true
+#  end
+#end
+
+
+  
