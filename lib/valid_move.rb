a8dab9976ed1849625c73503e956cbffb10c8a0c
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-14 18:24:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,13 +7,13 @@ require_relative '../spec/spec_helper.rb'
 #  end
 #end
 
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else 
-    true
-  end
-end
+#def position_taken?(board, index)
+ # if board[index] == " " || board[index] == "" || board[index] == nil
+  #  false
+  #else 
+   # true
+  #end
+#end
 
 
 
