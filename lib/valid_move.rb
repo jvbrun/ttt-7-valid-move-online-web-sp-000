f01d5675e53d4a25bad656d6d712a2f02554bf09
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-14 18:59:06 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,16 +2,11 @@ require_relative '../spec/spec_helper.rb'
 
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(0, 8)
-    true 
-    elsif position_taken?
-  end 
-end 
-
-# re-define your #position_taken? method here
-def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
     true 
   end
-end 
+end  
+
+# re-define your #position_taken? method here
+
 
